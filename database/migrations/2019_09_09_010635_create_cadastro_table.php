e28a3ed2402b80d982168aed9d5747f94cf87bba
hugo-meneses <hugo.menes@gmail.com>
2019-09-08 23:28:39 -0300

    cadastro de funcionarios

---
+++ database/migrations/2019_09_09_010635_create_cadastro_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCadastroTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cadastro', function (Blueprint $table) {
+            $table->increments('id');// Id da tabela (chave primária e incremento)
+            $table->string('nome');
+            $table->string('posto');
+            $table->string('pais');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
